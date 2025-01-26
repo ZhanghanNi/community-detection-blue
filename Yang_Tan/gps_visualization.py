3e303d654c89616cecdc54594ae2e041ed578a89
Yang-Tan-id <yangtan_stu@outlook.com>
2025-01-26 14:07:02 -0600

    update YT readme

--- Yang_Tan/gps_visualization.py
+++ Yang_Tan/gps_visualization.py
@@ -3,4 +3,28 @@ import geopandas
 import matplotlib.pyplot as plt
 from geodatasets import get_path
 
+#Source Code
+df = pd.DataFrame(
+    {
+        "City": ["Buenos Aires", "Brasilia", "Santiago", "Bogota", "Caracas"],
+        "Country": ["Argentina", "Brazil", "Chile", "Colombia", "Venezuela"],
+        "Latitude": [-34.58, -15.78, -33.45, 4.60, 10.48],
+        "Longitude": [-58.66, -47.91, -70.66, -74.08, -66.86],
+    }
+)
 
+gdf = geopandas.GeoDataFrame(
+    df, geometry=geopandas.points_from_xy(df.Longitude, df.Latitude), crs="EPSG:4326"
+)
+
+print(gdf.head())
+
+world = geopandas.read_file(get_path("naturalearth.land"))
+
+# We restrict to South America.
+ax = world.clip([-90, -55, -25, 15]).plot(color="white", edgecolor="black")
+
+# We can now plot our ``GeoDataFrame``.
+gdf.plot(ax=ax, color="red")
+
+plt.show()
--- Yang_Tan/readme.txt
+++ Yang_Tan/readme.txt
@@ -7,3 +7,8 @@
     Create / activate an empty conda environment, and then follow the install instructions:
     $ conda install -c conda-forge geopandas
     For code installation, check here: https://docs.anaconda.com/miniconda/install/#quick-command-line-install
+2. Moreover, you need 'geodatasets' package. Please instaill it with:
+    $conda install conda-forge::geodatasets
+
+3. And apparentlly you need fiona:
+    $ pip install fiona
