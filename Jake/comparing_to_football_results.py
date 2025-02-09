159984375fb115521e82c244b1b30dee5417f148
Jake Jasmer <jakejasmer@Jakes-MacBook-Pro.local>
2025-02-09 13:16:08 -0600

    testing on football dataset

---
+++ Jake/comparing_to_football_results.py
@@ -0,0 +1,47 @@
+import networkx as nx
+import sys
+sys.path.append("../Util/")
+sys.path.append("../Aidan/girvan_newman_implementation")
+import utils
+import girvan_newman as gn
+import girvan_newman as gni
+from collections import defaultdict
+
+
+def find_football_truth_values():
+    # Load the GML file
+    G = nx.read_gml("./football/football.gml")
+    
+    # Group nodes by their "value" (community)
+    community_dict = defaultdict(set)
+
+    for node, data in G.nodes(data=True):
+        community_dict[data["value"]].add(node)
+
+    # Convert to a list of sets
+    ground_truth_communities = list(community_dict.values())
+
+    out = ""
+    # Return the communities
+    for i, community in enumerate(ground_truth_communities, 1):
+        out += f"Community {i}: {community}" + "\n"
+    return out
+
+
+def find_girvan_newman_communities():
+    G = nx.read_gml("./football/football.gml")
+    solution = gni.main(G, True, "football")
+    print("--------------------------------------\nGirvan Newman Answers\n--------------------------------------")
+    out = ""
+    # Return the communities
+    for i, community in enumerate(solution, 1):
+        out += f"Community {i}: {community}" + "\n"
+    return out
+
+def main():
+    print(find_football_truth_values())
+    print(find_girvan_newman_communities)
+        
+
+if __name__ == "__main__":
+    main()
---
+++ Jake/running_bvns_on_footabll.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append("../Util")
+import utils
+import bvns_implementation as bvns
+import networkx as nx
+
+def main():
+    graph = nx.read_gml("./football/football.gml")
+    solution = bvns.bvns(graph, 4)
+    print("modularity: ", utils.modularity(graph, solution))
+    utils.plot_graph_with_communities(graph, solution)
+
+if __name__ == "__main__":
+    main()
