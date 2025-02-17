ace790a3eebcd161dd1b63a0e6ffd870ef134fea
Jake Jasmer <jakejasmer@Jakes-MacBook-Pro.local>
2025-02-17 09:18:45 -0600

    reorganized and added a better celegans graph

---
+++ Jake/neural_connectivity/README.md
@@ -0,0 +1,3 @@
+This folder contains work for making nx graphs with c elegans neuronal data. The one that I'd like
+to use is SI7_herm.csv which is made into an nx graph in csv_to_nx_graph.py and has labels. It
+comes from the paper found here: https://wormwiring.org/papers/Cook%20et%20al%202019.pdf
---
+++ Jake/neural_connectivity/csv_to_nx_graph.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import networkx as nx
+import sys
+sys.path.append("../../Util")
+import utils
+sys.path.append("../bvns")
+import bvns_implementation as bvns
+import matplotlib.pyplot as plt
+
+
+def main():
+    input_data = pd.read_csv('SI7_herm.csv', index_col=0) # Read the csv
+    input_data.columns = input_data.index # Get column and row names
+    input_data = input_data.apply(pd.to_numeric, errors="coerce").fillna(0) # Replace non-numeric entreis with 0
+    print(input_data)
+    G = nx.from_pandas_adjacency(input_data)
+    communities = bvns.bvns(G, 2, stop_criterion=5)
+    utils.plot_graph_with_communities(G, communities)
+
+
+if __name__ == "__main__":
+    main()
+
